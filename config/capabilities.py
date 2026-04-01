7a4527e944e7470a7f43db8d0773050ad10c7a51
Jano999 <99206814+Jano999@users.noreply.github.com>
2026-04-01 10:02:38 +0200

    Update capabilities.py
    commenting out not needed caps

--- config/capabilities.py
+++ config/capabilities.py
@@ -8,9 +8,9 @@ def get_caps():
         "appActivity": "com.motorola.cn.calculator.Calculator",
 
         "noReset": True,
-
-        "uiautomator2ServerLaunchTimeout": 60000,
-        "uiautomator2ServerInstallTimeout": 120000,
-        "adbExecTimeout": 120000,
-        "newCommandTimeout": 180
+        
+        #"uiautomator2ServerLaunchTimeout": 60000,
+        #"uiautomator2ServerInstallTimeout": 120000,
+        #"adbExecTimeout": 120000,
+        #"newCommandTimeout": 180
     }
